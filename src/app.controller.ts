4009b74b9d411763150d126cca9017f61547294a
rosenrose <crazytempler@gmail.com>
2022-09-27 17:04:58 +0900

    3.0 Introduction to Testing in Nest 3.1 Your first Unit Test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
@@ -6,9 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  home(@Req() req, @Res() res) {
-    console.log(req);
-    res.send("<h1>hello</h1>");
-    // 추천하지 않는 방법. NestJS는 Express와 Fastify와 호환 가능
+  home(): string {
+    return this.appService.home();
   }
 }
